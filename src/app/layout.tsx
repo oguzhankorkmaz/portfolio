41b25fef2e062a51e9549199d776e8a6f5c08255
oguzhan <oguzhannkorkmaz@gmail.com>
2025-12-12 20:13:41 +0300

    firebase google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,49 @@
 import Navbar from "@/components/Navbar/Navbar";
 import "../styles/globals.css";
+// app.js veya main.js dosyanızda
+
+import { initializeApp } from "firebase/app";
+
+import { getAnalytics } from "firebase/analytics";
 
 export const metadata = {
   title: "Oğuzhan Korkmaz - CV & Portfolio",
   description: "Oğuzhan Korkmaz - UI/UX Designer CV & Portfolio",
 };
 
+// TODO: Add SDKs for Firebase products that you want to use
 
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+
+// Your web app's Firebase configuration
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+const firebaseConfig = {
+
+  apiKey: "AIzaSyD4TL9y3czwzvw-hbvhNGD1auUvdRt9jWQ",
+
+  authDomain: "portfolio-28be8.firebaseapp.com",
+
+  projectId: "portfolio-28be8",
+
+  storageBucket: "portfolio-28be8.firebasestorage.app",
+
+  messagingSenderId: "1076242630116",
+
+  appId: "1:1076242630116:web:2d8980e991bf5270c773f1",
+
+  measurementId: "G-KZWV062V5N"
+
+};
+
+
+// Initialize Firebase
+
+const app = initializeApp(firebaseConfig);
+
+const analytics = getAnalytics(app);
 export default function RootLayout({
   children,
 }: Readonly<{
