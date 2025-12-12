793a0a580472f54b47c4e5636cdc7278ea1f94d8
oguzhan <oguzhannkorkmaz@gmail.com>
2025-12-12 21:19:32 +0300

    fix firebase analytics

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp, getApps } from "firebase/app";
 import { getAnalytics, isSupported } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN!,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET!,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID!,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID!,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID!,
+  apiKey: "AIzaSyD4TL9y3czwzvw-hbvhNGD1auUvdRt9jWQ",
+  authDomain: "designedbyoguzhan.com",
+  projectId: "portfolio-28be8",
+  storageBucket: "portfolio-28be8.firebasestorage.app",
+  messagingSenderId: "1076242630116",
+  appId: "1:1076242630116:web:2d8980e991bf5270c773f1",
+  measurementId: "G-KZWV062V5N"
 };
 
 export const app =
