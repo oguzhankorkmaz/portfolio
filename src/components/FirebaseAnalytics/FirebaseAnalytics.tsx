45ae7e9efdba20035bfc4c3ef464f3ba652fad5d
oguzhan <oguzhannkorkmaz@gmail.com>
2025-12-12 21:43:39 +0300

    fix firebase analytics

--- src/components/FirebaseAnalytics/FirebaseAnalytics.tsx
+++ src/components/FirebaseAnalytics/FirebaseAnalytics.tsx
@@ -10,13 +10,13 @@ import { useEffect } from "react";
 // Önemli: Bu bilgileri doğrudan buraya yazmak yerine, ortam değişkenlerinden almak daha güvenlidir.
 // Örneğin: process.env.NEXT_PUBLIC_FIREBASE_API_KEY
 const firebaseConfig = {
-  apiKey: "AIzaSyD4TL9y3czwzvw-hbvhNGD1auUvdRt9jWQ",
-  authDomain: "portfolio-28be8.firebaseapp.com",
-  projectId: "portfolio-28be8",
-  storageBucket: "portfolio-28be8.firebasestorage.app",
-  messagingSenderId: "1076242630116",
-  appId: "1:1076242630116:web:2d8980e991bf5270c773f1",
-  measurementId: "G-KZWV062V5N",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 let appInitialized = false; // Firebase uygulamasının yalnızca bir kez başlatıldığından emin olmak için
